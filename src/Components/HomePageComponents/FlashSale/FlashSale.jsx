d8ea44878a58df8249cb2ae382ce517720cbeb70
Mahmud-Dev63828 <mahmudul.hasan63828@gmail.com>
2025-06-10 16:17:44 +0600

    countdown added using worker

--- src/Components/HomePageComponents/FlashSale/FlashSale.jsx
+++ src/Components/HomePageComponents/FlashSale/FlashSale.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Heading from "../../CommonComponents/Heading";
+import Timer from "../../CommonComponents/Timer";
 
 const FlashSale = () => {
   return (
     <div className="container mx-auto">
       <Heading title={"Today"} description={"Flash Sale"} />
+      <Timer />
     </div>
   );
 };
