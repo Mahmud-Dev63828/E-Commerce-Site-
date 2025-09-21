8d37197ad4b94ec231539936f5ebef980036bccc
Mahmud-Dev63828 <mahmudul.hasan63828@gmail.com>
2025-09-22 03:53:03 +0600

    product page completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Link, Routes, BrowserRouter } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import { RootMainLayout } from "./Components/RootLyout/RootmainLayout";
+import ProductPage from "./Pages/Product/Index";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<RootMainLayout />}>
           <Route index element={<Home />}></Route>
+          <Route path="/product" element={<ProductPage />}></Route>
         </Route>
       </Routes>
     </BrowserRouter>
